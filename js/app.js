77d395ae1cd16f176c8548611a6930d4522f0622
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-03-01 03:37:07 +0600

    four commit

--- js/app.js
+++ js/app.js
@@ -32,3 +32,40 @@ const displaySearchResult = phones => {
     searchResult.appendChild(div);
   });
 }
+
+const loadPhoneDetail = id => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayPhoneDetail(data.data))
+}
+
+const displayPhoneDetail = phone => {
+  console.log(phone)
+  const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.textContent = '';
+  if(!phone.releaseDate){
+    const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `
+  <img class="img-fluid p-5" src="${phone.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${phone.name}</h5>
+    <h5 class="card-title">No release data found${phone.releaseDate}</h5>
+    <div>
+    <h5 class="card-title">Product details ${phone.name}</h5>
+    <li>Chip: ${phone.mainFeatures.chipSet}</li>
+    <li>Display Size: ${phone.mainFeatures.displaySize}</li>
+    <li>Memory: ${phone.mainFeatures.memory}</li>
+    <li>Storage: ${phone.mainFeatures.storage}</li>
+    <li>Bluetooth: ${phone.others.Bluetooth}</li>
+    <li>GPS: ${phone.others.GPS}</li>
+    <li>NFC: ${phone.others.NFC}</li>
+    <li>Radio: ${phone.others.Radio}</li>
+    <li>USB: ${phone.others.USB}</li>
+    <li>WLAN: ${phone.others.WLAN}</li>
+    </div> 
+  </div>
+  `;
+  phoneDetails.appendChild(div);
+  }
